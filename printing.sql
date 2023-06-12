3540d5568c6905da6734ee800a6602ec7a22f4cd
nsavi <112178048+nsavi@users.noreply.github.com>
2023-06-12 22:00:26 +0530

    Update printing.sql

--- printing.sql
+++ printing.sql
@@ -2,3 +2,15 @@ begin
 dbms_output.put_line('Welcome to Oracle PL-SQL');
 end;
 /
+--------------------------------
+declare
+var1 varchar2(10);
+num1 number(3);
+begin
+var1 := 'tutorials';
+num1 := 100;
+dbms_output.put_line('Val1 : ' || var1);
+dbms_output.put_line('Val2 : ' || num1);
+end;
+/
+--------------------------------------
