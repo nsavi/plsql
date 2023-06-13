b4ad22a62d27625e5b0cc76f5c7a86b9f23faa9b
nsavi <112178048+nsavi@users.noreply.github.com>
2023-06-13 06:28:51 +0530

    Create procedure.sql

---
+++ procedure.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE PROCEDURE test_procedure IS
+BEGIN 
+	dbms_output.put_line('Test procedure executed successfully');
+END;
+/
+
+execute test_procedure
+
+----------------------------------
+
